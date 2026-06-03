f1efeadb62422aa14f33cca9abf7423b8fa14fe9
enochguo1122 <enochguo1122@gmail.com>
2026-06-03 09:52:00 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import TeacherDashboard from "./TeacherDashboard";
+
+const isTeacher = window.location.pathname.startsWith("/teacher");
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {isTeacher ? <TeacherDashboard /> : <App />}
+  </StrictMode>
+);
